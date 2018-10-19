06c5dad9d3fbcdfe93082bbeb898dd5e2f1629a6
Adnan Faisal <afa@sitecore.net>
2018-10-19 15:10:44 -0400

    Fix for bug 287220.

---
+++ src/Sitecore.Support.287220/GetItemLinksModel.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using System.Collections.Generic;
+
+namespace Sitecore.Support
+{
+  public class GetItemLinksModel
+  {
+    public Item Argument { get; set; }
+
+    public List<object> List { get; set; }
+
+    public List<ID> List2 { get; set; }
+
+    public Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.DatasourceUsages.GetDatasourceUsagesCount Instance { get; set; }
+  }
+}
---
+++ src/Sitecore.Support.287220/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.287220")]
+[assembly: AssemblyProduct("Sitecore.Support.287220")]
+[assembly: ComVisible(false)]
